38571a71d4d0073fab42c36949f194a7789fc5d6
code-cody-418 <code_slayer1@protonmail.com>
2025-08-20 21:24:03 -0700

    changed query with limits and offsets and added item images

--- src/Items/ItemList.tsx
+++ src/Items/ItemList.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -15,6 +15,7 @@ const ItemRow = ({ item }: { item: WowItem }) => {
 
       <tr>
         <td>{item.item_id}</td>
+        <td><img src={item.media_value} alt={item.item_name} /></td>
         <td>
           <NavLink to={`/item/${item.item_id}`}>
             {item.item_name}
@@ -27,20 +28,29 @@ const ItemRow = ({ item }: { item: WowItem }) => {
 
 export const ItemList = () => {
   const [itemData, setItemData] = useState([])
+  const [searchString, setSearchString] = useState("")
   const [pagination, setPagination] = useState(0)
+  const [itemLimit] = useState(10)
 
-  const getItemData = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    try {
-      const response = await axios.get("http://localhost:4202/wow/get-wow-items/", {
-        params: {
-          search: e.target.value
-        }
-      })
-      setItemData(response.data.body)
-    } catch (error) {
-      console.error(error)
+
+
+  useEffect(() => {
+    const getItemData = async () => {
+      try {
+        const response = await axios.get("http://localhost:4202/wow/get-wow-items/", {
+          params: {
+            search: searchString,
+            itemLimit: itemLimit,
+            pageOffset: pagination
+          }
+        })
+        setItemData(response.data?.body)
+      } catch (error) {
+        console.error(error)
+      }
     }
-  }
+    getItemData()
+  }, [searchString, itemLimit, pagination])
 
   return (
     <>
@@ -50,20 +60,25 @@ export const ItemList = () => {
         </Row>
         <Row>
           <Form.Label>Item Search</Form.Label>
-          <Form.Control onChange={(e: React.ChangeEvent<HTMLInputElement>) => getItemData(e)} type="search" placeholder="Search Item" />
+          <Form.Control onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setSearchString(e.target?.value)
+            setPagination(0)
+          }
+          } type="search" placeholder="Search Item" />
         </Row>
         <Row>
           <Table striped>
             <thead>
               <tr>
                 <th>Item ID</th>
+                <th>Item Image</th>
                 <th>Item Name</th>
               </tr>
             </thead>
             <tbody>
               {
                 itemData ?
-                  itemData.slice(pagination, pagination + 5).map((item: WowItem) => (
+                  itemData.map((item: WowItem) => (
                     <ItemRow key={item.item_id} item={item} />
                   ))
                   : null
@@ -74,12 +89,12 @@ export const ItemList = () => {
         <Row>
           <Col>
             {
-              pagination > 0 ? <Button onClick={() => setPagination(pagination - 5)}>Previous</Button> : null
+              pagination > 0 ? <Button onClick={() => setPagination(pagination - itemLimit)}>Previous</Button> : null
             }
           </Col>
           <Col>
             {
-              itemData?.length > 0 ? <Button onClick={() => setPagination(pagination + 5)}>Next</Button> : null
+              itemData?.length > 0 ? <Button onClick={() => setPagination(pagination + itemLimit)}>Next</Button> : null
             }
           </Col>
         </Row>
--- src/Items/WowItem.interface.ts
+++ src/Items/WowItem.interface.ts
@@ -1,4 +1,5 @@
 export interface WowItem {
   item_id: string;
   item_name: string;
+  media_value: string;
 }
